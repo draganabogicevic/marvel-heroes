5b204020688cb8f2b00b26ccee4cea9afb6eeeb7
BBogicevic <branislav.bogicevic@deluxe.com>
2021-09-04 13:10:54 +0200

    small changes on Comic.js

--- src/entities/Comic.js
+++ src/entities/Comic.js
@@ -1,11 +1,11 @@
 class Comic {
     constructor(title, photo, price, printDate, creators, characters) {
-    this.title = title,
-    this.photo = photo,
-    this.price = price,
-    this.printDate = new Date (printDate),
-    this.creators = creators,
-    this.characters = characters
+    this.title = title;
+    this.photo = photo;
+    this.price = price;
+    this.printDate = new Date (printDate);
+    this.creators = creators;
+    this.characters = characters;
     }
 
 }
